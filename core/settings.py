9b2dcb02483e1f17157890c579c4606fde1b867f
Heitor Queiroga Duarte <heitorqueiroga1@hotmail.com>
2023-11-19 17:17:28 -0300

    database config settings

--- core/settings.py
+++ core/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "paglaumdaoback",
+        "USER": "django",
+        "PASSWORD": "heitorelindoegostoso",
+        "HOST": "localhost",
+        "PORT": '',
     }
 }
 
